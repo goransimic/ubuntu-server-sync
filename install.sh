6daedf2e3fb4d23d4b0a8b5459662f60bfde8d45
Goran Simic <goransimicdev@gmail.com>
2025-02-23 00:24:47 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo apt-get update > /dev/null
 sudo apt-get install -y git > /dev/null
 mkdir -p $HOME/.local/{bin,share}
 rm -rf $HOME/.local/{bin,share}/ubuntu-server-sync
-git clone https://github.com/goransimicdev/ubuntu-server-sync.git $HOME/.local/share/ubuntu-server-sync
+git clone https://github.com/goransimic/ubuntu-server-sync.git $HOME/.local/share/ubuntu-server-sync
 ln -sv $HOME/.local/share/ubuntu-server-sync/sync.sh $HOME/.local/bin/ubuntu-server-sync
